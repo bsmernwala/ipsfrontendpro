6a31e75d962698c10fe4d5528459c5722848a4ae
bsmernwala <bsmernwala@gamil.com>
2026-06-24 17:40:52 +0530

    vercel updates

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AdminLogin from './AdminViews/AdminLogin';
-import StateMgt from './AdminViews/StateMgt';
-import ProductCatgMgt from './AdminViews/ProductCatgMgt';
-import CityMgt from './AdminViews/CityMgt';
-import VendorReg from './vendorviews/VendorReg';
-import VenderLogin from './vendorviews/VenderLogin';
-import CustomerReg from './CustomerViews/CustomerReg';
-import CustomerLogin from './CustomerViews/CustomerLogin';
-import Product from './ProductViews/Product';
-import ProductListMain from "./ProductViews/ProductListforMainPage"
-import ProductListforMainPage from './ProductViews/ProductListforMainPage';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
